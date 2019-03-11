958a6d06aefba24bb92e4954311eb0d6505ccaad
vanshkapoor <vanshkapoorvk7@gmail.com>
2019-03-11 08:42:38 +0530

    auth and backend

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI:'mongodb://vansh:a1a2a3a4@ds163354.mlab.com:63354/devbvp',
+	secret:'secret'
+
+};
---
+++ server.js
@@ -0,0 +1,61 @@
+const express     = require('express');
+const mongoose    = require('mongoose');
+const database    = require('./config/keys').mongoURI;
+const bodyParser  = require('body-parser');
+const passport    = require('passport');
+
+//api routes
+const user      = require('./routes/api/user');
+const profile   = require('./routes/api/profile');
+const posts     = require('./routes/api/posts');
+
+const app = express();
+
+//body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//mlab connection
+mongoose.connect(database,{ useNewUrlParser: true },function(err){
+	if(err){
+		console.log(err);
+	}else{
+		console.log("connected to database");
+	}
+});
+
+app.get('/',(req,res)=>{
+	res.send('check!!');
+});
+
+//passport middleware
+app.use(passport.initialize());
+
+//passport config
+require('./config/passport')(passport);
+
+//use routes
+app.use('/api/user', user);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+
+
+
+
+
+
+
+
+
+//serving
+const port = process.env.PORT || 5000;
+app.listen(port ,(err)=>{
+	if(err){
+		console.log(err);
+	}else
+	{
+		console.log('server running on port 5000');
+	}
+})
